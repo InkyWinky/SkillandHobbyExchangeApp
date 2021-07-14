a32feb7d33a4ceef12a29083aca35eae7c57d1eb
Taylor <taylorramnarong@gmail.com>
2021-07-14 13:19:31 +0700

    Added basic chatbox functionality, requires tweaking.

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./styles/ChatBox.css"
-import io from 'socket.io'
+import io from 'socket.io-client'
 
-const ChatBox = () => {
-    const socket = io('http://localhost:5000')
+const ChatBox = (props) => {
 
-    socket.on('chat-message', data => {
-        console.log(data)
-    })
+    const [socket, setSocket] = useState(io('http://localhost:5000', { transports : ['websocket'] }))
+    const [message, setMessage] = useState("")
+    const [messageList, setMessageList] = useState([])
+
+    useEffect(() => {
+        socket.on('chat-message', data => {
+            const tempMsgList = Array.from(messageList);
+            tempMsgList.push(data)
+            setMessageList(tempMsgList);
+    })}, [])
+
+
+
+    const handleMessageSubmit = (e) =>{
+        e.preventDefault()
+        socket.emit('send-chat-message', message)
+        setMessage("")
+    }
+
 
 
     return (
         <div id="chatBox">
-        <form id="send-container">
-            <input type="text" id="message-input"></input>
+            {messageList.map((item, index) => {return(<h6 key={item}>{item}</h6>)})}
+        <form id="send-container" onSubmit={handleMessageSubmit}>
+            <input type="text" id="message-input" value={message} onChange={(e) => setMessage(e.target.value)}></input>
                 <button type="submit" id="send-button">
                     Send
                 </button>
         </form>
+        <button onClick={() => {console.log(messageList)}}>Click Me!</button>
 
     </div>
     )
