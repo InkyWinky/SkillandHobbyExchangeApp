86e9224b12bd40d6d7f36ed4a1bee0f4d21c9349
InkyWinky <46879999+InkyWinky@users.noreply.github.com>
2021-07-15 23:26:41 +1000

    Revert "Landing page react app that works"
    This reverts commit fae1e3b1ece86182273dba0add349b55f1922151.

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
 import logo from './logo.svg';
+import Prelogin from './components/Prelogin'
+import Register from "./components/Register"
+import DirectMessagePage from "./components/DirectMessagePage"
+import RegisterSecondPage from "./components/RegisterSecondPage"
+import RegisterThirdPage from "./components/RegisterThirdPage"
 import './App.css';
-import React from 'react';
+import db from './firebase.config';
+import Profile from './components/Profile';
+// For testing purposes.
 
-class App extends React.Component{
-  render(){
+
+
+
+function App() {
   return (
     <div className="App">
-  <div id = "butter">
-            <nav className="navbar">
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Discover</a></li>
-                    <li><a href="">Matches</a></li>
-                    <li><a href="">Friends</a></li>
-                    <li><a href="">Profile</a></li>
-                </ul>
-            </nav>
-            <div className="whynot">
-                <div className = "start">
-                    <div className="title"><h1 className = "titleHeading">HobEx</h1>
-                    <p className= "subheading">The power of <br/> exchange.</p></div>
-<video id="earthVideo" src="0001-0250.mp4"></video>
-</div>
-</div>
-   
-        <div className="login-div">
-            <img src="basketballsmall.png" alt="basketball" className="ballImage"/>
-       
-            <div className="signup-words"><p>Learn <span className="pink">anything</span> you want</p></div>
-            <p className="inExchangeFor">in exchange for</p>
-     
-    </div>
-        <div className="signup-div">
-            <img src="soccerBallsmall.png" alt="soccer-ball" className = "ballImage"/>
-        <div className="signup-words"><p>teaching something you <span className="pink">love.</span></p></div>
-    </div>
-
-        <div className="parallax">
-            <div className="parallaxLayer parallaxLayer0">
-                <img className = "footerImg" src="parallaxfooterBack.png" />    
-
-            </div>
-            <div className="parallaxLayer parallaxLayer1">
-                <img className = "footerImg" src="parallaxfooterMidBack.png" />    
-                
-            </div>
-            <div className=" parallaxLayer parallaxLayer2">
-                <img className = "footerImg" src="parallaxfootermiddle.png" />
-            </div>
-            <div className="parallaxLayer parallaxLayer3">
-                <img className = "footerImg" src="parallaxfooterFront.png" />
-            </div>
-            <div className="parallaxBase"><p className = "signUpPersuation">Start learning today</p>
-            <button className = "signUpBtn">Sign up</button></div>
-            
-        </div>
-        
-    
-        <p className ="footer">Copyright 2021. All Rights Reserved. </p>
-</div>
-
+      
+      <Profile/>
+      
     </div>
   );
-  }
 }
 
 export default App;
